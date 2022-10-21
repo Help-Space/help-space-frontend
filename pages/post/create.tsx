45f9c302e67cfb3a3bd7fe564707e32d91945258
Mateusz <75246331+Zikuz@users.noreply.github.com>
2022-10-21 22:22:41 +0200

    Add CreatePostPage

---
+++ pages/post/create.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import CreatePostForm from "./CreatePostForm";
+
+const CreatePost: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>HelpSpace | Dodaj ogłoszenia</title>
+                <link rel="icon" href="/helpspace_logo_icon.svg" />
+            </Head>
+            <main className="flex flex-col gap-[7rem] items-center px-[25%] pt-[4rem] pb-[8rem]  xl:px-[15%] md:px-[10%] sm:pt-[2rem] sm:gap-[5rem] sm:pb-[5rem] sm:px-[5%]">
+                <div className="flex flex-col gap-5  justify-center items-center">
+                    <span className="text-[32px] font-[700] sm:text-[28px]">Dodaj ogłoszenie!</span>
+                    <span className="text-center sm:text-[15px]">Lorem Ipsum is simply dummy text of the printing and printer. Lorem Ipsum is simply dummy text of the printing and printer. Lorem Ipsum is simply dummy text of the printing and printer.</span>
+                </div>
+                <CreatePostForm />
+            </main>
+        </>
+    );
+};
+
+export default CreatePost;
