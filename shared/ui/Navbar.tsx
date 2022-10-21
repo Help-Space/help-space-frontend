5ded791e32b6a79e80619d74ea674d74701e2fae
Mateusz <75246331+Zikuz@users.noreply.github.com>
2022-10-21 18:16:22 +0200

    Add Tailwind

--- shared/ui/Navbar.tsx
+++ shared/ui/Navbar.tsx
@@ -1,5 +1,27 @@
 import { Navbar as NextNavbar } from "@nextui-org/react";
+import Link from "next/link";
 
 export default function Navbar() {
-    return <NextNavbar></NextNavbar>;
+    return (
+        <NextNavbar shouldHideOnScroll>
+            <NextNavbar.Brand>
+                <Link href="/">
+                    <div>
+                        <img src="/helpspace_logo.svg" alt="logo" className="cursor-pointer xs:hidden" />
+                        <img src="/helpspace_logo_icon2.svg" alt="logo" className="cursor-pointer hidden xs:block w-[30px]" />
+                    </div>
+                </Link>
+            </NextNavbar.Brand>
+
+            <NextNavbar.Content>
+                <Link href="/login">
+                    Zaloguj się
+                </Link> 
+            </NextNavbar.Content>
+            
+        </NextNavbar>
+    );
 }
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xxl: { max: "1600px"},
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      mdd: { max: "967px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+      xs: { max: "439px" },
+  },
+    colors: {
+      'primaryPink': '#FF6969',
+      'secondaryPink': '#FFCBCB',
+      'mediumDark': '#353535',
+      'lightDark': '#606060',
+      'white': '#fff',
+    },
+    
+    fontFamily: {
+      "quicksand": ['Quicksand'],
+      "poppins": ['Poppins'],
+    },
+    plugins: [],
+  }
+}
