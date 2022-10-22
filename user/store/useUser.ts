816522ed735a244f1ad426eb58981ba2bf032c51
Artiu <71273256+Artiu@users.noreply.github.com>
2022-10-23 00:36:15 +0200

    Fix hydration error

--- user/store/useUser.ts
+++ user/store/useUser.ts
@@ -11,10 +11,10 @@ interface UserState {
     lastName: string;
     email: string;
     created_at: Date;
-    logIn: (email: string, password: string) => void;
-    logOut: () => void;
-    register: (user: RegisterUserRequest) => void;
-    load: () => void;
+    logIn: (email: string, password: string) => Promise<void>;
+    logOut: () => Promise<void>;
+    register: (user: RegisterUserRequest) => Promise<void>;
+    load: () => Promise<void>;
 }
 
 const initialState = {
@@ -27,84 +27,73 @@ const initialState = {
     created_at: new Date(),
 };
 
-export const useUser = create<UserState>()(
-    persist(
-        (set) => ({
-            ...initialState,
-            async logIn(email, password) {
-                if (!email || !password) {
-                    throw new Error("Email i hasło nie mogą być puste");
-                }
-                const user: GetUserResponse = await fetchApi(
-                    "/user/login",
-                    { email, password },
-                    { method: "POST" }
-                );
-                set({
-                    isLoggedIn: true,
-                    id: user._id,
-                    username: user.username,
-                    firstName: user.first_name,
-                    lastName: user.last_name,
-                    email: user.email,
-                });
-            },
-            async logOut() {
-                await fetchApi("/user/logout", undefined, { method: "DELETE" });
-                set(initialState);
-            },
-            async register(user) {
-                if (
-                    !user.email ||
-                    !user.firstName ||
-                    !user.lastName ||
-                    !user.birthDate ||
-                    !user.password
-                ) {
-                    throw new Error("Wszystkie pola muszą być wypełnione!");
-                }
+export const useUser = create<UserState>()((set) => ({
+    ...initialState,
+    async logIn(email, password) {
+        if (!email || !password) {
+            throw new Error("Email i hasło nie mogą być puste");
+        }
+        const user: GetUserResponse = await fetchApi(
+            "/user/login",
+            { email, password },
+            { method: "POST" }
+        );
+        set({
+            isLoggedIn: true,
+            id: user._id,
+            username: user.username,
+            firstName: user.first_name,
+            lastName: user.last_name,
+            email: user.email,
+        });
+    },
+    async logOut() {
+        await fetchApi("/user/logout", undefined, { method: "DELETE" });
+        set(initialState);
+    },
+    async register(user) {
+        if (!user.email || !user.firstName || !user.lastName || !user.birthDate || !user.password) {
+            throw new Error("Wszystkie pola muszą być wypełnione!");
+        }
 
-                const userRes: GetUserResponse = await fetchApi(
-                    "/user/register",
-                    {
-                        email: user.email,
-                        password: user.password,
-                        first_name: user.firstName,
-                        last_name: user.lastName,
-                        birth_date: new Date(user.birthDate),
-                    },
-                    {
-                        method: "POST",
-                    }
-                );
-                set({
-                    isLoggedIn: true,
-                    id: userRes._id,
-                    username: userRes.username,
-                    firstName: userRes.first_name,
-                    lastName: userRes.last_name,
-                    email: user.email,
-                });
+        const userRes: GetUserResponse = await fetchApi(
+            "/user/register",
+            {
+                email: user.email,
+                password: user.password,
+                first_name: user.firstName,
+                last_name: user.lastName,
+                birth_date: new Date(user.birthDate),
             },
-            async load() {
-                let user: GetUserResponse;
-                try {
-                    user = await fetchApi("/user");
-                } catch (_) {
-                    set(initialState);
-                    return;
-                }
-                set({
-                    isLoggedIn: true,
-                    id: user._id,
-                    username: user.username,
-                    firstName: user.first_name,
-                    lastName: user.last_name,
-                    email: user.email,
-                    created_at: user.created_at,
-                });
-            },
-        }),
-        { name: "user-storage" }
-    )
-);
+            {
+                method: "POST",
+            }
+        );
+        set({
+            isLoggedIn: true,
+            id: userRes._id,
+            username: userRes.username,
+            firstName: userRes.first_name,
+            lastName: userRes.last_name,
+            email: user.email,
+        });
+    },
+    async load() {
+        let user: GetUserResponse;
+        try {
+            user = await fetchApi("/user");
+        } catch (_) {
+            set(initialState);
+            return;
+        }
+        set({
+            isLoggedIn: true,
+            id: user._id,
+            username: user.username,
+            firstName: user.first_name,
+            lastName: user.last_name,
+            email: user.email,
+            created_at: user.created_at,
+        });
+    },
+}));
