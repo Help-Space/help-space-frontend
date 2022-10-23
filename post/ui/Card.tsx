f66ced3ee5981271cd02236ef5c89f027777e231
Mateusz <75246331+Zikuz@users.noreply.github.com>
2022-10-23 03:04:49 +0200

    Add Dropdown Option

--- post/ui/Card.tsx
+++ post/ui/Card.tsx
@@ -1,11 +1,42 @@
 import { Post } from "post/types";
-import { Button, Card, Spacer, Tooltip } from "@nextui-org/react";
+import { Button, Card, Spacer, Tooltip, Dropdown } from "@nextui-org/react";
 import HeartIcon from "./HeartIcon";
 import { postApi } from "post/api";
 import { useMemo, useState } from "react";
 import { useUser } from "user/store/useUser";
 import { useRouter } from "next/router";
 import UserAvatar from "user/ui/Avatar";
+
+function DropdownMenu({ id } : { id: string }) {
+    
+    return (
+        <Dropdown>
+            <Dropdown.Button color="error" flat icon={<img src="/dropdown_icon.svg" alt="icon" />} ></Dropdown.Button>
+            <Dropdown.Menu>
+                <Dropdown.Item key="refresh">
+                    {/* <div onClick={() => postApi.refresh(id)}> */}
+                        Odśwież ogłoszenie
+                    {/* </div> */}
+                </Dropdown.Item>
+                <Dropdown.Item key="close">
+                    {/* <div onClick={() => postApi.update()> */}
+                        Zakończ ogłoszenie
+                    {/* </div> */}
+                </Dropdown.Item>
+                <Dropdown.Item key="edit">
+                    {/* <div> */}
+                        Edytuj ogłoszenie
+                    {/* </div> */}
+                </Dropdown.Item>
+                <Dropdown.Item key="delete" withDivider color="error">
+                    {/* <div onClick={() => postApi.remove(id)}> */}
+                        Usuń ogłoszenie
+                    {/* </div> */}
+                </Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
 
 function ButtonConv() {
     return (
@@ -65,23 +96,29 @@ export default function PostCard({
             >
                 <span
                     className="font-[700] text-[20px] cursor-pointer md:text-[18px]"
+                    style={{ display: 'flex', flexWrap: 'wrap', wordBreak: 'break-word' }} 
                     onClick={() => router.push(`/post/${postId}`)}
                 >
                     {title}
                 </span>
-                <div>
-                    <Tooltip
-                        content={"Musisz być zalogowany, by móc dodawać ogłoszenia do ulubionych"}
-                        css={{ display: isLoggedIn ? "none" : "block" }}
-                        className="self-end"
-                    >
-                        <Button
-                            onClick={isLoggedIn ? like : undefined}
-                            auto
-                            icon={<HeartIcon fill={isLiked ? "#d4402c" : "black"} />}
-                        />
-                    </Tooltip>
-                </div>
+                {authorId !== userId && isLoggedIn ? (
+                        <div>
+                            <Tooltip
+                                content={"Musisz być zalogowany, by móc dodawać ogłoszenia do ulubionych"}
+                                css={{ display: isLoggedIn ? "none" : "block" }}
+                                className="self-end"
+                            >
+                                <Button
+                                    onClick={isLoggedIn ? like : undefined}
+                                    auto
+                                    icon={<HeartIcon fill={isLiked ? "#d4402c" : "black"} />}
+                                />
+                            </Tooltip>
+                        </div>
+                    ) : (
+                        <DropdownMenu id={postId} />
+                    )
+                }
             </div>
             <div>
                 <div className="flex font-quicksand" style={{ paddingInline: "1.5rem" }}>
